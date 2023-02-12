b13658776c7c821f3fb6aa3788fb563a184b467b
Mohd Saif Ansari (TERMINATOR) <124523961+mohdsaif0026@users.noreply.github.com>
2023-02-12 22:55:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-python3 update.py && python3 -m bot
+pip3 install aria2p
+
+pip3 install yt-dlp
+
+pip3 install speedtest-cli
+
+pip3 install qbittorrent-api
+
+
+python3 update.py && python3 -m botpip3 install aria2p
+
+
+
